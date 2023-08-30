418435d0b6302d0fa647d85b224a0956a19578bd
miguelmc <mcatedral24@gmail.com>
2023-08-30 09:21:33 -0600

    Mensajes Flash

--- hello.py
+++ hello.py
@@ -1,8 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
-app.debug=True
-
+app.config['SECRET_KEY']="mySuperSecretKey"
+#Create a form Class:
+class NamerForm(FlaskForm):
+    name=StringField("what's your name", validators=[DataRequired()])
+    submit=SubmitField("Submit") 
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -10,7 +16,16 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template("user.html", nombre=name)
-
+@app.route('/name', methods=['GET','POST'])
+def name():
+    name=None
+    form=NamerForm()
+    # Validate Form
+    if form.validate_on_submit():
+        name=form.name.data
+        form.name.data=''
+        flash("Form Submitted Successfully!")
+    return render_template("name.html", name=name, form=form)
 #Create Custom Error Pages
 #Invalid URL
 @app.errorhandler(404)
@@ -21,4 +36,12 @@ def page_not_found(e):
 def page_not_found(e):
     return render_template("500.html"), 500
 #git:
-#I_could_have_done_that
+#I_could_have_done_that}
+# BooleanField, DateField, DateTimeField, DecimalField, FileField, HiddenField, MultipleField, FieldList, FloatField, FormField, IntegerField, PassworldField, RadioField, SelectField, SelectMultipleField, SubmitField, StringField, TextAreaField 
+
+"""
+Validators: DataRequired, email, EqualTo, InputRequired, IPAddress, Length, MacAddress, NumberRange, Optional, Regexp, UUID (numero de identificación de usuario únicof), AnyOf, NoneOf
+"""
+
+
+#revise: https://www.bogotobogo.com/python/Flask/Python_Flask_Blog_App_with_MongoDB.php
